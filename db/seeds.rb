32f8ac0b032e46aac585fc44edcabbefd0b768c6
amusictheorist <a_millaresthomson@outlook.com>
2025-01-14 11:51:37 -0500

    Set up database, migrations, and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,77 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Create users
+puts 'Seeding database'
+puts 'Creating users'
+user1 = User.create!(
+  email: 'a@a.com',
+  password: 'asdf'
+)
+
+user2 = User.create!(
+  email: 'b@b.com',
+  password: 'qwer'
+)
+
+user3 = User.create!(
+  email: 'c@c.com',
+  password: 'zxcv'
+)
+
+user4 = User.create!(
+  email: 'd@d.com',
+  password: '1234'
+)
+
+user5 = User.create!(
+  email: 'e@e.com',
+  password: '4567'
+)
+
+user6 = User.create!(
+  email: 'f@f.com',
+  password: '7890'
+)
+puts "Users created"
+
+# Create urls
+puts 'Creating URLs'
+Url.create!(
+  short_url: 'b2xVn2',
+  long_url: 'http://www.lighthouselabs.ca',
+  user: user1
+)
+
+Url.create!(
+  short_url: '9sm5xK',
+  long_url: 'http://www.google.com',
+  user: user2
+)
+
+Url.create!(
+  short_url: 'q2evqwzpg3',
+  long_url: 'http://www.youtube.com',
+  user: user3
+)
+
+Url.create!(
+  short_url: 'au8lus7d05',
+  long_url: 'http://www.wikipedia.com',
+  user: user4
+)
+
+Url.create!(
+  short_url: 'nbmwdt7us2o',
+  long_url: 'http://www.github.com',
+  user: user5
+)
+
+Url.create!(
+  short_url: 'piforyrfd',
+  long_url: 'http://www.outlook.com',
+  user: user6
+)
+puts 'URLs created'
+puts 'Databse seeded successfully'
